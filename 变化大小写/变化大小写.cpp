ef2f2bef3c9926447018695ddc8539aa6da337cf
yjy12138 <1220366265@qq.com>
2020-02-17 22:03:40 +0800

    先用双层循环按照字母顺序把所有字母排好序保存在vector1中，然后遍历原来的字符串，如果是字母就从vector1中从前往后输出1个字母。否则就输出遍历到的符号

---
+++ 变化大小写/变化大小写.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+bool isenglish(char a)
+{
+	if ((a <= 90 && a >=65) || (a <= 122&& a >=97))return true;
+	else return false;
+}
+int main()
+{
+	vector<char> abc;
+	string input;
+	getline(cin, input);
+	vector<int >index;
+	int len = input.length();
+	for (char indexbig = 65, indexsmall = 97; indexbig <= 90 && indexsmall <= 122; indexbig++, indexsmall++)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if (input[i] == indexbig || input[i] == indexsmall)
+			{
+				abc.push_back(input[i]);
+			}
+		}
+	}
+	
+	for (int i = 0,k=0; i < len&&k<abc.size(); i++)
+	{
+		if (isenglish(input[i]))
+		{
+			cout << abc[k];
+			k++;
+		}
+		else
+		cout << input[i];
+	}
+}

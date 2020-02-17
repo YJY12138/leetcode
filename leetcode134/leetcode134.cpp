5ee07f2c581a4acb598789566ded14ac4c8439f3
yjy12138 <1220366265@qq.com>
2020-02-17 20:59:34 +0800

    leetcode136 hash

---
+++ leetcode134/leetcode134.cpp
@@ -0,0 +1,60 @@
+﻿#include<stdc++.h>
+#include<vector>
+#include<queue>
+using namespace std;
+int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+	queue<int >costline;
+	queue<int >gasline;
+	int n = gas.size();
+	for (int i = 0; i < n; i++)
+	{
+		costline.push(cost[i]);
+		gasline.push(gas[i]);
+	}
+	vector<int > states;
+	int flag = 0;
+	for (int i = 0; i <n; i++)
+	{
+		cout << "这是从" << i << "出发" << endl;
+		int currentgas = 0;
+		cout << "当前gasline队头：" << gasline.front() << endl;
+		cout << "当前costline队头：" << costline.front()<< endl;
+		cout << gasline.size() << endl;
+		cout << costline.size() << endl;
+		for (int j = 0; j < n; j++)
+		{			
+			int temp1 = gasline.front();
+			currentgas += temp1;
+			int temp2 = costline.front();		
+			costline.pop();
+			gasline.pop();
+			costline.push(temp2);
+			gasline.push(temp1);
+			
+			if (currentgas - temp2 < 0)
+			{
+				states.push_back(-1);
+				flag = -1;
+				break;
+			}
+			else { 
+				flag = i; currentgas = currentgas - temp2;
+			cout << currentgas<<endl;
+			}
+	
+		}
+		if (flag != -1) {
+			flag = i; 
+		}
+	}
+	return flag;
+}
+int main()
+{
+
+	int gas[5] = {1,2,3,4,5};
+	int cost[] = {3,4,5,1,2};
+	vector<int> b(cost, cost + 5);
+	vector<int> a(gas, gas + 5);
+	cout << canCompleteCircuit(a, b) << endl;
+}
---
+++ leetcode136/leetcode136.cpp
@@ -0,0 +1,49 @@
+﻿#include<stdc++.h>
+using namespace std;
+int flag[100000] = {0};
+/*int singleNumber(vector<int>& nums) {//用数组下标记录次数无法解决负数问题。
+	int max = 0;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		if (max < nums[i])
+			max = nums[i];
+		flag[nums[i]] += 1;
+	}
+	for (int i = 0; i <= max; i++)
+	{
+		if (flag[i] % 2 != 0)
+			return i;
+	}
+}*/
+int singleNumber(vector<int>& nums) {//异或交换律
+	
+	int ans = nums[0];
+	if (nums.size() > 1) {
+		for (int i = 1; i < nums.size(); i++) {
+			ans = ans ^ nums[i];
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	int n;
+
+	while (cin>>n)
+	{
+		int f = 0;
+		vector<int > num;
+		for (int i = 0; i < n; i++)
+		{
+			scanf_s("%d", &f);
+			num.push_back(f);
+		}
+		for (int i = 0; i < n; i++)
+		{
+			cout << num[i] << ' ';
+		}
+		cout << endl;
+		cout << singleNumber(num) << endl;
+	}
+}
